38d7923330c8e1cb215bfc4dc46175144474dfd9
DEEPAK <deepakrathore2k1@gmail.com>
2021-11-21 00:18:49 +0530

    add

--- app.js
+++ app.js
@@ -15,13 +15,17 @@ const { generateFile } = require('./generateFile');
 const { executeCpp } = require('./executeCpp');
 
 app.post("/run", async (req, res) => {
-    var {lang, code} = req.body;
-    if(!lang || !code) {
-        return res.status(400).json({success: false, error: "Code or language cannot be empty"});
+    try {
+        var {lang, code} = req.body;
+        if(!lang || !code) {
+            return res.status(400).json({success: false, error: "Code or language cannot be empty"});
+        }
+        var filePath = await generateFile(lang, code);
+        var output = await executeCpp(filePath);
+        return res.json({ output });
+    } catch(e) {
+        console.log(e);
     }
-    var filePath = await generateFile(lang, code);
-    var output = await executeCpp(filePath);
-    return res.json({ output });
 })
 // --------------------------------
 app.get("/", async (req, res) => {
--- executeCpp.js
+++ executeCpp.js
@@ -9,18 +9,26 @@ if (!fs.existsSync(outputPath)) {
 
 const executeCpp = async (filePath) => {
     const jobId = path.basename(filePath).split('.')[0];
-    const outPath = path.join(outputPath, `${jobId}.exe`);
+    const outPath = path.join(outputPath, `${jobId}.out`);
     console.log('jobId', jobId)
     console.log('filePath', filePath)
     console.log('outPath', outPath)
     console.log(__dirname)
-    return new Promise(async(resolve, reject) => {
+    return new Promise(async (resolve, reject) => {
         // Fix the cmd to compile cpp file
-        exec(`g++ "${filePath}" -o "${outPath}" && start "${outPath}"`, (error, stdout, stderr) => {
+        // cd "g:\code editor\code-editor-backend\codes\" ; if ($?) { g++ 1c3c730a-5697-4cb3-a23c-9ce7a5b1ca1b.cpp -o 1c3c730a-5697-4cb3-a23c-9ce7a5b1ca1b } ; if ($?) { .\1c3c730a-5697-4cb3-a23c-9ce7a5b1ca1b }
+        // exec(`g++ "${filePath}" -o "${outPath}"`, (error, stdout, stderr) => {
+        //     exec(`start ${outPath}`, (error, stdout, stderr) => {
+        //         error && reject({ error, stderr });
+        //         stderr && reject({ stderr });
+        //         resolve(stdout)
+        //     })
+        // });
+        exec(`g++ "${filePath}" -o "${outPath}" && cd "${outputPath}" && ./${jobId}.out`, (error, stdout, stderr) => {
             error && reject({ error, stderr });
             stderr && reject({ stderr });
-            resolve(stdout);
-        });
+            resolve(stdout)
+        })
     })
 }
 
